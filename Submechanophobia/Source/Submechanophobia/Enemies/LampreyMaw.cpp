c0ccdd165891a860c47cab572028a71bf1478fa0
LiMiAnNo <123137537+LiMiAnNo@users.noreply.github.com>
2025-02-21 18:20:57 -0500

    commented out constructor for lamprey maw enemy
    should fix crashing  for now

--- Submechanophobia/Source/Submechanophobia/Enemies/LampreyMaw.cpp
+++ Submechanophobia/Source/Submechanophobia/Enemies/LampreyMaw.cpp
@@ -5,23 +5,23 @@
 
 ALampreyMaw::ALampreyMaw()
 {
-	// Change these when we have the proper model 
-	CapsuleComponent->SetRelativeScale3D(FVector(3.066639f, 1.0, 2.818257));
-	CapsuleComponent->SetRelativeRotation(FRotator(90.0f, 90.0f, 180.0f));
-	CapsuleComponent->SetRelativeLocation(FVector(14.1915f, 0.0f, 58.972f));
+	//// Change these when we have the proper model 
+	//CapsuleComponent->SetRelativeScale3D(FVector(3.066639f, 1.0, 2.818257));
+	//CapsuleComponent->SetRelativeRotation(FRotator(90.0f, 90.0f, 180.0f));
+	//CapsuleComponent->SetRelativeLocation(FVector(14.1915f, 0.0f, 58.972f));
 
-	static ConstructorHelpers::FObjectFinder<USkeletalMesh> enemyAsset(TEXT("/Game/Boss_assets/Lamprey_Maw/LaMa_exp_feb3.LaMa_exp_feb3"));
-	if (enemyAsset.Succeeded())
-	{
-		// Set the mesh object, scale and location 
-		enemyMesh->SetSkeletalMesh(enemyAsset.Object);
-		enemyMesh->SetWorldScale3D(FVector(10.0f,30.0f,10.0f));
-		enemyMesh->SetWorldRotation(FRotator(0.0f, 0.0f, -90.0f));
-		enemyMesh->SetRelativeLocation(FVector(0.0f, 0.0f, -10.0f));
+	//static ConstructorHelpers::FObjectFinder<USkeletalMesh> enemyAsset(TEXT("/Game/Boss_assets/Lamprey_Maw/LaMa_exp_feb3.LaMa_exp_feb3"));
+	//if (enemyAsset.Succeeded())
+	//{
+	//	// Set the mesh object, scale and location 
+	//	enemyMesh->SetSkeletalMesh(enemyAsset.Object);
+	//	enemyMesh->SetWorldScale3D(FVector(10.0f,30.0f,10.0f));
+	//	enemyMesh->SetWorldRotation(FRotator(0.0f, 0.0f, -90.0f));
+	//	enemyMesh->SetRelativeLocation(FVector(0.0f, 0.0f, -10.0f));
 
-		
-		
-		
+	//	
+	//	
+	//	
 		/*// Enable Physics 
 		enemyMesh->SetSimulatePhysics(true);
 		enemyMesh->SetCollisionProfileName("Pawn");
