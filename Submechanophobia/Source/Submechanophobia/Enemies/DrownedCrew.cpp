406fc6694d9658ee7d342403e8af3ff215de9f01
Bmorado <BrndnMorado@gmail.com>
2025-04-07 11:58:40 -0400

    disabled steam session worked on enemy behavior tree

--- Submechanophobia/Source/Submechanophobia/Enemies/DrownedCrew.cpp
+++ Submechanophobia/Source/Submechanophobia/Enemies/DrownedCrew.cpp
@@ -12,8 +12,6 @@ ADrownedCrew::ADrownedCrew()
 	CapsuleComponent->SetRelativeRotation(FRotator(0.0f, 0.0f, 0.0f));
 	CapsuleComponent->SetRelativeLocation(FVector(0.0f, 0.0f, 0.0f));
 
-	MovementComponent->MaxSpeed = 400;
-	
 	static ConstructorHelpers::FObjectFinder<USkeletalMesh> enemyAsset(TEXT("/Game/Boss_assets/crewmate1/crewmate1_GEO.crewmate1_GEO"));
 	if (enemyAsset.Succeeded())
 	{
